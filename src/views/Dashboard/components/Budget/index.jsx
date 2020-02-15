3f1ce6751a4126656b3379090289606e7c0979df
yayanheeh <tokoxp58@gmail.com>
2020-02-15 12:11:19 +0700

    Update index.jsx

--- src/views/Dashboard/components/Budget/index.jsx
+++ src/views/Dashboard/components/Budget/index.jsx
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+
 
 // Externals
 import classNames from 'classnames';
@@ -35,9 +41,34 @@ class Budget extends Component {
       >
         <div className={classes.content}>
        
-          <div className={classes.iconWrapper}>
-            <div class="banner"></div>   </div>
-        </div>
+              <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>  </div>
         
       </Paper>
     );
