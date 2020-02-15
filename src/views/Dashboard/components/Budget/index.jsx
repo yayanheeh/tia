5a4b9a6e9e8a3d6aa3fa811e9ad3941577be3fc6
yayanheeh <tokoxp58@gmail.com>
2020-02-15 23:40:55 +0700

    Update index.jsx

--- src/views/Dashboard/components/Budget/index.jsx
+++ src/views/Dashboard/components/Budget/index.jsx
@@ -1,60 +1,31 @@
 import React, { Component } from 'react';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-
-
-// Externals
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
-
-// Material helpers
-import { withStyles } from '@material-ui/core';
-
-// Material components
-import { Typography } from '@material-ui/core';
-
-// Material icons
-import {
-  ArrowDownward as ArrowDownwardIcon,
-  Money as MoneyIcon
-} from '@material-ui/icons';
-
-// Shared components
-import { Paper } from 'components';
-
 // Component styles
 import styles from './styles';
 
-class Budget extends Component {
-  render() {
-    const { classes, className, ...rest } = this.props;
 
-    const rootClassName = classNames(classes.root, className);
-
-    return (
-      
-           
-              
-      
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="140"
-          image="https://cdn.pixabay.com/photo/2019/06/22/18/26/woman-4292185_1280.jpg"
-          title="Contemplative Reptile"
-        />
-        
-    
-    );
-  }
+function ResponsiveImage( { src, width, height } ) {
+  return (
+    <div
+      style={ { 
+        width,
+      } }
+      className="responsive-image">
+      <div style={ {
+          paddingBottom: ( height / width * 100 ) + '%'
+        } } />
+      <img
+        src={ src }
+        className="responsive-image__image" />
+    </div>
+  );
 }
 
-Budget.propTypes = {
-  className: PropTypes.string,
-  classes: PropTypes.object.isRequired
-};
+ReactDOM.render(
+  <ResponsiveImage
+    src="https://lorempixel.com/1200/800/"
+    width={ 1200 }
+    height={ 800 } />,
+  document.getElementById( 'app' )
+);
 
-export default withStyles(styles)(Budget);
+
