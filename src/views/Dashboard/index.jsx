16b7e9e4136d0abc0a97f23389d8b3baa6fd556b
yayanheeh <tokoxp58@gmail.com>
2020-02-16 21:41:29 +0700

    Update index.jsx

--- src/views/Dashboard/index.jsx
+++ src/views/Dashboard/index.jsx
@@ -61,7 +61,7 @@ class Dashboard extends Component {
    </Grid>
             <Grid
               item
-              md={10}
+              md={9}
               xs={12}
             >
                   <OrdersTable className={classes.item} />
