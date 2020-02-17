1662bdf9717fd7cb6f76ce22b23a0f8248be07b5
yayanheeh <tokoxp58@gmail.com>
2020-02-17 20:54:28 +0700

    Update index.jsx

--- src/layouts/Dashboard/components/Footer/index.jsx
+++ src/layouts/Dashboard/components/Footer/index.jsx
@@ -3,10 +3,8 @@ import React from 'react';
 import { render } from 'react-dom';
 // Component styles
 import styles from './styles.css';
-const Footer = () => (
+
   <footer className="footer">
     <p>Some footer nonsense!</p>
   </footer>
-);
-
 
