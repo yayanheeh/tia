090edeea1511193ad66d036c7e62d006c437b87d
yayanheeh <tokoxp58@gmail.com>
2020-02-17 18:51:23 +0700

    Update index.jsx

--- src/layouts/Dashboard/components/Footer/index.jsx
+++ src/layouts/Dashboard/components/Footer/index.jsx
@@ -9,9 +9,7 @@ import LocationOnIcon from '@material-ui/icons/LocationOn';
 const useStyles = makeStyles({
   root: {
    position: fixed,
-   left: 0,
-   bottom: 0,
-   width: 100%,
+   width: 100,
   },
 });
 
