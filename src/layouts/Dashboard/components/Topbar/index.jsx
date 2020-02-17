58a441642f05ed14171206c3d93eb8392849bf92
yayanheeh <tokoxp58@gmail.com>
2020-02-17 10:37:47 +0700

    Update index.jsx

--- src/layouts/Dashboard/components/Topbar/index.jsx
+++ src/layouts/Dashboard/components/Topbar/index.jsx
@@ -2,7 +2,6 @@ import React, { Component, Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
 import Hidden from '@material-ui/core/Hidden';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
 // Externals
