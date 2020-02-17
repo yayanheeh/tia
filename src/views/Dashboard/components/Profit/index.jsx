8e67fe11b2260fc5ab50e1752172d4b0790c3cfa
yayanheeh <tokoxp58@gmail.com>
2020-02-17 10:21:58 +0700

    Update index.jsx

--- src/views/Dashboard/components/Profit/index.jsx
+++ src/views/Dashboard/components/Profit/index.jsx
@@ -2,58 +2,53 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import 
+        </Grid>
+      </Grid>
+    
+    
+    
+    
+  );
+}
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-const useStyles = makeStyles({
+
+const useStyles = makeStyles(theme => ({
   root: {
-    maxWidth: 345,
+    flexGrow: 1,
   },
-  media: {
-    height: 140,
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
   },
-  
+}));
 
-});
-
-export default function MediaCard() {
+export default function NestedGrid() {
   const classes = useStyles();
 
+  function FormRow() {
+    return (
+      <React.Fragment>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="https://cdn.pixabay.com/photo/2015/12/05/06/20/kid-1077793__480.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-      
-     </Card>
-
-
-<Grid container spacing={1}>
+    <div className={classes.root}>
+      <Grid container spacing={1}>
         <Grid container item xs={12} spacing={3}>
           <FormRow />
         </Grid>
@@ -64,9 +59,7 @@ export default function MediaCard() {
           <FormRow />
         </Grid>
       </Grid>
-    
-    
-    
-    
+    </div>
   );
 }
+Li
