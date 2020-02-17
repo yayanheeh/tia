9718fc0c2f431dc02ffe12013fa8d28d0c94cc49
yayanheeh <tokoxp58@gmail.com>
2020-02-17 17:38:46 +0700

    Update index.jsx

--- src/views/Dashboard/components/OrdersTable/index.jsx
+++ src/views/Dashboard/components/OrdersTable/index.jsx
@@ -15,7 +15,22 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import FolderIcon from '@material-ui/icons/Folder';
+import FlightTakeoff from '@material-ui/icons/FlightTakeoff';
+import DateRange from '@material-ui/icons/DateRange';
+import Store from '@material-ui/icons/Store';
+import DirectionsWalk from '@material-ui/icons/DirectionsWalk';
+import AccessAlarms from '@material-ui/icons/AccessAlarms';
+import StarBorder from '@material-ui/icons/StarBorder';
 
+
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
 const useStyles = makeStyles(theme => ({
   root: {
     maxWidth: 345,
@@ -60,65 +75,69 @@ export default function RecipeReviewCard() {
             <MoreVertIcon />
           </IconButton>
         }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title="Paket Umroh Bulan Mei"
+        subheader="Harga Rp 20.000.000"
       />
-      <CardMedia
-        className={classes.media}
-        image="/static/images/cards/paella.jpg"
-        title="Paella dish"
+      
+                  
+      
+      <ListItem>
+                  <ListItemIcon>
+                    <FlightTakeoff/>
+                  </ListItemIcon>
+                  <ListItemText
+                    primary="Maskapai" />
+        <ListItemText
+                    primary={<Typography type="secondary" style={{ align: 'left'  }}>Saudi airlines</Typography>}
       />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-        </CardContent>
-      </Collapse>
+                            
+                </ListItem>
+      <ListItem>
+                  <ListItemIcon>
+                    <Store />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary="Kelas hotel" />
+        <ListItemText
+                    primary={<Typography type="secondary" style={{ align: 'left'  }}><StarBorder/><StarBorder/><StarBorder/> </Typography>}
+      />
+                            
+                </ListItem>
+      <ListItem>
+                  <ListItemIcon>
+                    <AccessAlarms />
+                  </ListItemIcon>
+                  <ListItemText>
+                    Berangkat dari
+                   </ListItemText>
+                   <ListItemText>
+                    jakarta
+                   </ListItemText>
+                            
+                </ListItem>
+      <ListItem>
+                  <ListItemIcon>
+                    <DirectionsWalk />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary="Durasi perjalanan" />
+        <ListItemText
+                    primary={<Typography type="secondary" style={{ align: 'left' }}>20 hari</Typography>}
+      />
+                            
+                </ListItem>
+      <ListItem>
+                  <ListItemIcon>
+                    <DateRange />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary="Jadwal berangkat" />
+        <ListItemText
+                    primary={<Typography type="secondary" style={{ align: 'left' }}>11 Mei 2020</Typography>}
+      />
+                            
+                </ListItem>
+      
     </Card>
   );
 }
