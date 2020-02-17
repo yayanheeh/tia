f3771150410d41c3db0f4eabcf72d596b262da6f
yayanheeh <tokoxp58@gmail.com>
2020-02-17 09:42:07 +0700

    Update index.jsx

--- src/views/Dashboard/components/Profit/index.jsx
+++ src/views/Dashboard/components/Profit/index.jsx
@@ -16,6 +16,14 @@ const useStyles = makeStyles({
   media: {
     height: 140,
   },
+  flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  
 });
 
 export default function MediaCard() {
