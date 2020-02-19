76b455c0b04ac690e5f958153b957188ff5ff270
yayanheeh <tokoxp58@gmail.com>
2020-02-19 13:44:27 +0700

    Update index.jsx

--- src/views/Video/Components/YouTube/index.jsx
+++ src/views/Video/Components/YouTube/index.jsx
@@ -2,28 +2,54 @@
 import React, { Component } from 'react';
 
 // Component styles
-import style from './style';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-
-
-var YouTube = React.createClass({
-  render: function() {
-    var videoSrc = "https://www.youtube.com/embed/" + 
-        this.props.video + "?autoplay=" + 
-        this.props.autoplay + "&rel=" + 
-        this.props.rel + "&modestbranding=" +
-        this.props.modest;
-    return (
-      <div className="container">
-        <iframe className="player" type="text/html" width="100%" height="100%"
-  src={videoSrc}
-  frameborder="0"/>
-      </div>
-    );
-  }
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
 });
 
-React.render(
-  <YouTube video="mYFaghHyMKc" autoplay="0" rel="0" modest="1" />,
-  document.body
-);
+export default function MediaCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
