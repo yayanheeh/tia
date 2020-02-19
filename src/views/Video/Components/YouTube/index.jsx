f88098692b97f7405be0d9e4e8667573f6f4b748
yayanheeh <tokoxp58@gmail.com>
2020-02-19 13:25:14 +0700

    Update index.jsx

--- src/views/Video/Components/YouTube/index.jsx
+++ src/views/Video/Components/YouTube/index.jsx
@@ -1,1 +1,21 @@
-Tes
+var YouTube = React.createClass({
+  render: function() {
+    var videoSrc = "https://www.youtube.com/embed/" + 
+        this.props.video + "?autoplay=" + 
+        this.props.autoplay + "&rel=" + 
+        this.props.rel + "&modestbranding=" +
+        this.props.modest;
+    return (
+      <div className="container">
+        <iframe className="player" type="text/html" width="100%" height="100%"
+  src={videoSrc}
+  frameborder="0"/>
+      </div>
+    );
+  }
+});
+
+React.render(
+  <YouTube video="mYFaghHyMKc" autoplay="0" rel="0" modest="1" />,
+  document.body
+);
