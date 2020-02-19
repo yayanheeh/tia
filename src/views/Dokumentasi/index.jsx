25aaa66838df4debee5db8a13bb71f7daf85a378
yayanheeh <tokoxp58@gmail.com>
2020-02-19 16:21:23 +0700

    Update index.jsx

--- src/views/Dokumentasi/index.jsx
+++ src/views/Dokumentasi/index.jsx
@@ -1,39 +1,62 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
-// Externals
-import PropTypes from 'prop-types';
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    maxWidth: 500,
+  },
+});
 
+export default function Types() {
+  const classes = useStyles();
 
-class Dokumentasi extends Component {
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <DashboardLayout title="Typography">
-        <div className={classes.root}>
-
-
-          
-<h4>Dokumen Persyaratan Umroh</h4>
-            
-       <Typography variant="body2" gutterBottom>
-          Pas foto dengan latar belakang putih 4 x 6 sebanyak 6 lembar
-</Typography>
-            <Typography variant="body2" gutterBottom>
-       Paspor asli yang masih berlaku dengan susunan jumlah nama 3 kata
-</Typography>
-  
-
-        </div>
-      </DashboardLayout>
-    );
-  }
+  return (
+    <div className={classes.root}>
+      <Typography variant="h1" component="h2" gutterBottom>
+        h1. Heading
+      </Typography>
+      <Typography variant="h2" gutterBottom>
+        h2. Heading
+      </Typography>
+      <Typography variant="h3" gutterBottom>
+        h3. Heading
+      </Typography>
+      <Typography variant="h4" gutterBottom>
+        h4. Heading
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        h5. Heading
+      </Typography>
+      <Typography variant="h6" gutterBottom>
+        h6. Heading
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom>
+        subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+      </Typography>
+      <Typography variant="subtitle2" gutterBottom>
+        subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+        unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
+        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+      </Typography>
+      <Typography variant="body2" gutterBottom>
+        body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+        unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
+        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+      </Typography>
+      <Typography variant="button" display="block" gutterBottom>
+        button text
+      </Typography>
+      <Typography variant="caption" display="block" gutterBottom>
+        caption text
+      </Typography>
+      <Typography variant="overline" display="block" gutterBottom>
+        overline text
+      </Typography>
+    </div>
+  );
 }
-
-Dokumentasi.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(Dokumentasi);
