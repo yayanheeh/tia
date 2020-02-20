10ec6ea31fe115714429388e074707cef7b8136f
yayanheeh <tokoxp58@gmail.com>
2020-02-20 14:17:59 +0700

    Update index.jsx

--- src/views/Tes/index.jsx
+++ src/views/Tes/index.jsx
@@ -1,1 +1,19 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+class MyHeader extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Hello Style!</h1>
+      <p>Add a little style!.</p>
+      </div>
+    );
+   
+}
+}
+
+ReactDOM.render(<MyHeader />, document.getElementById('root'));
+
