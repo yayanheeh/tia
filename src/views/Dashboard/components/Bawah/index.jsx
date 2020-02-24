5c2edb2c4bebf06e44d5697340d30c1d842ded43
yayanheeh <tokoxp58@gmail.com>
2020-02-24 14:45:37 +0700

    Update index.jsx

--- src/views/Dashboard/components/Bawah/index.jsx
+++ src/views/Dashboard/components/Bawah/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@bit/mui-org.material-ui.styles';
-import GridList from '@bit/mui-org.material-ui.grid-list';
-import GridListTile from '@bit/mui-org.material-ui.grid-list-tile';
-import GridListTileBar from '@bit/mui-org.material-ui.grid-list-tile-bar';
-import IconButton from '@bit/mui-org.material-ui.icon-button';
-import StarBorderIcon from '@bit/mui-org.material-ui-icons.star-border';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 import tileData from './tileData';
 
 const useStyles = makeStyles(theme => ({
@@ -16,23 +16,23 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.background.paper,
   },
   gridList: {
-    width: 500,
-    height: 450,
+    flexWrap: 'nowrap',
     // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
     transform: 'translateZ(0)',
   },
-  titleBar: {
-    background: 'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' + 'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  title: {
+    color: theme.palette.primary.light,
   },
-  icon: {
-    color: 'white',
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
   },
 }));
 
 /**
  * The example data is structured as follows:
  *
- *
+ * import image from 'path/to/image.jpg';
  * [etc...]
  *
  * const tileData = [
@@ -40,32 +40,32 @@ const useStyles = makeStyles(theme => ({
  *     img: image,
  *     title: 'Image',
  *     author: 'author',
- *     featured: true,
  *   },
  *   {
  *     [etc...]
  *   },
  * ];
  */
-export default function AdvancedGridList() {
+export default function SingleLineGridList() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root} style={{width: 'auto', height: 'auto'}}>
-      <GridList cellHeight={200} spacing={1} className={classes.gridList}>
+    <div className={classes.root}>
+      <GridList className={classes.gridList} cols={2.5}>
         {tileData.map(tile => (
-          <GridListTile key={tile.img} cols={tile.featured ? 2 : 1} rows={tile.featured ? 2 : 1}>
+          <GridListTile key={tile.img}>
             <img src={tile.img} alt={tile.title} />
             <GridListTileBar
               title={tile.title}
-              titlePosition="top"
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
               actionIcon={
-                <IconButton aria-label={`star ${tile.title}`} className={classes.icon}>
-                  <StarBorderIcon />
+                <IconButton aria-label={`star ${tile.title}`}>
+                  <StarBorderIcon className={classes.title} />
                 </IconButton>
               }
-              actionPosition="left"
-              className={classes.titleBar}
             />
           </GridListTile>
         ))}
