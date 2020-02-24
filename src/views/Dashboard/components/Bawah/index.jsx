8049850d9e3f10829b886b5c04088d7318a0d58a
yayanheeh <tokoxp58@gmail.com>
2020-02-24 14:35:52 +0700

    Update index.jsx

--- src/views/Dashboard/components/Bawah/index.jsx
+++ src/views/Dashboard/components/Bawah/index.jsx
@@ -1,60 +1,75 @@
-import React, { Component } from 'react';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import React from 'react';
+import { makeStyles } from '@bit/mui-org.material-ui.styles';
+import GridList from '@bit/mui-org.material-ui.grid-list';
+import GridListTile from '@bit/mui-org.material-ui.grid-list-tile';
+import GridListTileBar from '@bit/mui-org.material-ui.grid-list-tile-bar';
+import IconButton from '@bit/mui-org.material-ui.icon-button';
+import StarBorderIcon from '@bit/mui-org.material-ui-icons.star-border';
+import tileData from './tileData';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+  },
+  titleBar: {
+    background: 'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' + 'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+  icon: {
+    color: 'white',
+  },
+}));
 
-// Externals
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
+/**
+ * The example data is structured as follows:
+ *
+ *
+ * [etc...]
+ *
+ * const tileData = [
+ *   {
+ *     img: image,
+ *     title: 'Image',
+ *     author: 'author',
+ *     featured: true,
+ *   },
+ *   {
+ *     [etc...]
+ *   },
+ * ];
+ */
+export default function AdvancedGridList() {
+  const classes = useStyles();
 
-// Material helpers
-import { withStyles } from '@material-ui/core';
-
-// Material components
-import { Typography } from '@material-ui/core';
-
-// Material icons
-import {
-  ArrowDownward as ArrowDownwardIcon,
-  Money as MoneyIcon
-} from '@material-ui/icons';
-
-// Shared components
-import { Paper } from 'components';
-
-
-class Bawah extends Component {
-  render() {
-    const { classes, className, ...rest } = this.props;
-
-    const rootClassName = classNames(classes.root, className);
-
-    return (
-      
-              <div className={classes.root}>
-      
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="310"
-          width="1000"
-          image="https://kiblatq.com/wp-content/uploads/2019/07/cropped-KiblatQ-Masjidil-Haram-Header-1024x315.jpg"
-          title="Contemplative Reptile"
-        />
-        
-    </div> 
-        
-      
-    );
-  }
+  return (
+    <div className={classes.root} style={{width: 'auto', height: 'auto'}}>
+      <GridList cellHeight={200} spacing={1} className={classes.gridList}>
+        {tileData.map(tile => (
+          <GridListTile key={tile.img} cols={tile.featured ? 2 : 1} rows={tile.featured ? 2 : 1}>
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              titlePosition="top"
+              actionIcon={
+                <IconButton aria-label={`star ${tile.title}`} className={classes.icon}>
+                  <StarBorderIcon />
+                </IconButton>
+              }
+              actionPosition="left"
+              className={classes.titleBar}
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
 }
-
-Bawah.propTypes = {
-  className: PropTypes.string,
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(Bawah);
