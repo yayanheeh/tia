6a123ddd001adff6a0930c223eae95763d1a6bdb
yayanheeh <tokoxp58@gmail.com>
2020-02-24 14:23:45 +0700

    Create index.jsx

---
+++ src/views/Dashboard/components/Bawah/index.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+
+
+// Externals
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+// Material helpers
+import { withStyles } from '@material-ui/core';
+
+// Material components
+import { Typography } from '@material-ui/core';
+
+// Material icons
+import {
+  ArrowDownward as ArrowDownwardIcon,
+  Money as MoneyIcon
+} from '@material-ui/icons';
+
+// Shared components
+import { Paper } from 'components';
+
+
+class Bawah extends Component {
+  render() {
+    const { classes, className, ...rest } = this.props;
+
+    const rootClassName = classNames(classes.root, className);
+
+    return (
+      
+              <div className={classes.root}>
+      
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="310"
+          width="1000"
+          image="https://kiblatq.com/wp-content/uploads/2019/07/cropped-KiblatQ-Masjidil-Haram-Header-1024x315.jpg"
+          title="Contemplative Reptile"
+        />
+        
+    </div> 
+        
+      
+    );
+  }
+}
+
+Bawah.propTypes = {
+  className: PropTypes.string,
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(Bawah);
