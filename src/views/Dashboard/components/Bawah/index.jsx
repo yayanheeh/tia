933d5e372d74a6b609e745b6c2c8bddfad3dda3a
yayanheeh <tokoxp58@gmail.com>
2020-02-24 15:27:41 +0700

    Update index.jsx

--- src/views/Dashboard/components/Bawah/index.jsx
+++ src/views/Dashboard/components/Bawah/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-
+import Box from '@material-ui/core/Box';
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
@@ -19,7 +19,8 @@ export default function CenteredGrid() {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={1}>
+  <Box bgcolor="text.secondary" color="background.paper" p={2}>
+         <Grid container spacing={1}>
         
         <Grid item xs={3}>
           <Paper className={classes.paper}>xs=3</Paper>
@@ -33,7 +34,7 @@ export default function CenteredGrid() {
         <Grid item xs={3}>
           <Paper className={classes.paper}>xs=3</Paper>
         </Grid>
-      </Grid>
+      </Grid></Box>
     </div>
   );
 }
