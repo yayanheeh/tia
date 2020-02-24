6dd1539605fc02efa1a1fe1159666a400cbe6b4b
yayanheeh <tokoxp58@gmail.com>
2020-02-24 16:08:49 +0700

    Update index.jsx

--- src/views/Dashboard/components/Bawah/index.jsx
+++ src/views/Dashboard/components/Bawah/index.jsx
@@ -6,6 +6,7 @@ import Box from '@material-ui/core/Box';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import { Link } from 'react-router-dom';
+import ListItemText from '@material-ui/core/ListItemText';
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
