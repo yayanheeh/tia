fa138b91497cec0d3c7e48b15d60776ecf54a3c5
yayanheeh <tokoxp58@gmail.com>
2020-02-24 13:56:57 +0700

    Update index.jsx

--- src/views/Dashboard/components/Footer/index.jsx
+++ src/views/Dashboard/components/Footer/index.jsx
@@ -1,41 +1,62 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
 
-export default function NestedGrid() {
-  const classes = useStyles();
+// Externals
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-  function FormRow() {
-   
+// Material helpers
+import { withStyles } from '@material-ui/core';
 
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={1}>
-        <Grid container item xs={12} spacing={3}>
-          <FormRow />
-        </Grid>
-        <Grid container item xs={12} spacing={3}>
-          <FormRow />
-        </Grid>
-        <Grid container item xs={12} spacing={3}>
-          <FormRow />
-        </Grid>
-        <Grid container item xs={12} spacing={3}>
-          <FormRow />
-        </Grid>
-      </Grid>
-    </div>
-  );
+// Material components
+import { Typography } from '@material-ui/core';
+
+// Material icons
+import {
+  ArrowDownward as ArrowDownwardIcon,
+  Money as MoneyIcon
+} from '@material-ui/icons';
+
+// Shared components
+import { Paper } from 'components';
+
+// Component styles
+import styles from './styles';
+
+class Footer extends Component {
+  render() {
+    const { classes, className, ...rest } = this.props;
+
+    const rootClassName = classNames(classes.root, className);
+
+    return (
+      
+              <div className={classes.root}>
+      
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="310"
+          width="1000"
+          image="https://kiblatq.com/wp-content/uploads/2019/07/cropped-KiblatQ-Masjidil-Haram-Header-1024x315.jpg"
+          title="Contemplative Reptile"
+        />
+        
+    </div> 
+        
+      
+    );
+  }
 }
+
+Footer.propTypes = {
+  className: PropTypes.string,
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Footer);
