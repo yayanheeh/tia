f110192098217662824d53aa3a0901fd846d4962
yayanheeh <tokoxp58@gmail.com>
2020-02-24 14:49:09 +0700

    Update index.jsx

--- src/views/Dashboard/components/Bawah/index.jsx
+++ src/views/Dashboard/components/Bawah/index.jsx
@@ -5,8 +5,9 @@ import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import IconButton from '@material-ui/core/IconButton';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
-import tileData from './tileData';
 
+
+import { titleData } from './titleData';
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
