8c45699456aaac9ce22bf6dd4db0438ea0e9d11e
yayanheeh <tokoxp58@gmail.com>
2020-02-25 18:13:37 +0700

    Update index.jsx

--- src/views/Profil/index.jsx
+++ src/views/Profil/index.jsx
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
-
-import { MetaTags } from './components/Meta';
+import React from 'react';
+import MetaTags from './components/Meta';
 
 class Profil extends React.Component {
   render() {
     return (
-      
-        <MetaTags>
-          <title>React Meta Tags | Page1</title>
-          <link rel="canonical" href="/profil" />
-          <meta name="description" content="React meta tags handles document meta/head tags in isomorphic react with ease." />
-          <meta property="og:title" content="MyApp-Page1" />
-          <meta itemProp="test" content="Profil" />
-        </MetaTags>
-        
-          
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          
-        
-      
-    )
+        <div className="wrapper">
+          <MetaTags>
+            <title>yan Page 1</title>
+            <meta name="description" content="Some description." />
+            <meta property="og:title" content="MyApp" />
+            <meta property="og:image" content="path/to/image.jpg" />
+          </MetaTags>
+          <div className="content"> Some Content </div>
+        </div>
+      )
   }
 }
-
-export default Profil;
