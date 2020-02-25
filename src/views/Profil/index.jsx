7efbbf7e72fb7a6eec0ad3ed89343824d2c6bf6d
yayanheeh <tokoxp58@gmail.com>
2020-02-25 17:53:43 +0700

    Update index.jsx

--- src/views/Profil/index.jsx
+++ src/views/Profil/index.jsx
@@ -8,7 +8,7 @@ class Profil extends React.Component {
       <div className="Profil">
         <MetaTags>
           <title>React Meta Tags | yayan</title>
-          <link rel="canonical" href="/page1" />
+          <link rel="canonical" href="/profil" />
           <meta name="description" content="React meta tags handles document meta/head tags in isomorphic react with ease." />
           <meta property="og:title" content="MyApp-Page1" />
           <meta itemProp="test" content="Profil" />
@@ -18,7 +18,7 @@ class Profil extends React.Component {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </div>
-      </div>
+      
     )
   }
 }
