1a2ea761f2bfbded7b2a02bdf42f4853e0f9a063
yayanheeh <tokoxp58@gmail.com>
2020-02-25 17:12:56 +0700

    Update index.jsx

--- src/views/Profil/index.jsx
+++ src/views/Profil/index.jsx
@@ -5,13 +5,13 @@ class Profil extends React.Component {
   render() {
     return (
       <div className="page1">
-        <MetaTags>
+        <Meta>
           <title>React Meta Tags | Page1</title>
           <link rel="canonical" href="/page1" />
           <meta name="description" content="React meta tags handles document meta/head tags in isomorphic react with ease." />
           <meta property="og:title" content="MyApp-Page1" />
           <meta itemProp="test" content="Page1" />
-        </MetaTags>
+        </Meta>
         <div>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
