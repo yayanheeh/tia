486ec3571a6550b71acabc794e9befba0a20a077
yayanheeh <tokoxp58@gmail.com>
2020-02-25 18:08:05 +0700

    Update index.jsx

--- src/views/Profil/index.jsx
+++ src/views/Profil/index.jsx
@@ -13,11 +13,11 @@ class Profil extends React.Component {
           <meta property="og:title" content="MyApp-Page1" />
           <meta itemProp="test" content="Profil" />
         </MetaTags>
-        <div>
-          <p>
+        
+          
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
+          
+        
       
     )
   }
