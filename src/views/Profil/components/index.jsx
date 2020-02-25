8f93fc36c62863f5fe9eb2dd0b6626469ba29035
yayanheeh <tokoxp58@gmail.com>
2020-02-25 17:39:56 +0700

    Update index.jsx

--- src/views/Profil/components/index.jsx
+++ src/views/Profil/components/index.jsx
@@ -1,1 +1,6 @@
-export { default as Meta } from './Meta';
+import MetaTagsContext from './meta_tags_context';
+import MetaTags from './meta_tags';
+import ReactTitle from './react_title';
+
+export default MetaTags;
+export {MetaTags, MetaTagsContext, ReactTitle};
