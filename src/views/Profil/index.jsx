7a8b1cd2e0857918753f9e59194c44fac43f7eb7
yayanheeh <tokoxp58@gmail.com>
2020-02-25 18:05:31 +0700

    Update index.jsx

--- src/views/Profil/index.jsx
+++ src/views/Profil/index.jsx
@@ -5,7 +5,7 @@ import { MetaTags } from './components/Meta';
 class Profil extends React.Component {
   render() {
     return (
-      <div className="profil">
+      
         <MetaTags>
           <title>React Meta Tags | Page1</title>
           <link rel="canonical" href="/profil" />
@@ -18,7 +18,7 @@ class Profil extends React.Component {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </div>
-      </div>
+      
     )
   }
 }
