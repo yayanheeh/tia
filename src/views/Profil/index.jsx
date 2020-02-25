9993f35f514589039dcc1526e4930ff0d34692aa
yayanheeh <tokoxp58@gmail.com>
2020-02-25 16:26:13 +0700

    Update index.jsx

--- src/views/Profil/index.jsx
+++ src/views/Profil/index.jsx
@@ -1,6 +1,23 @@
 import React, { Component, Fragment } from 'react';
 
+import React from 'react';
+import MetaTags from 'react-meta-tags';
 
+class Component1 extends React.Component {
+  render() {
+    return (
+        <div class="wrapper">
+          <MetaTags>
+            <title>Page 1</title>
+            <meta id="meta-description" name="description" content="Some description." />
+            <meta id="og-title" property="og:title" content="MyApp" />
+            <meta id="og-image" property="og:image" content="path/to/image.jpg" />
+          </MetaTags>
+          <div class="content"> Some Content </div>
+        </div>
+      )
+  }
+}
 
 // Externals
 import PropTypes from 'prop-types';
