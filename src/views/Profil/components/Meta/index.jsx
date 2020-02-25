17e52abacbbe9a6af45bc11c79305b1f90edf033
yayanheeh <tokoxp58@gmail.com>
2020-02-25 17:39:25 +0700

    Delete index.jsx

--- src/views/Profil/components/Meta/index.jsx
+++
@@ -1,6 +0,0 @@
-import MetaTagsContext from './meta_tags_context';
-import MetaTags from './meta_tags';
-import ReactTitle from './react_title';
-
-export default MetaTags;
-export {MetaTags, MetaTagsContext, ReactTitle};
