b313368e39ccbd13b54b8a0d556a6425857ac4a5
yayanheeh <tokoxp58@gmail.com>
2020-02-26 11:10:04 +0700

    Update index.js

--- src/views/Youtube/index.js
+++ src/views/Youtube/index.js
@@ -1,11 +1,38 @@
-import React, { PureComponent } from "react"
+import React, { Component,PureComponent } from 'react';
+
 import ReactDOM from "react-dom"
 import Iframe from "./iframe.js"
 
-class Youtube extends PureComponent {
-	render() {
-		return (
-			<Iframe
+// Externals
+import PropTypes from 'prop-types';
+
+// Material helpers
+import { withStyles } from '@material-ui/core';
+
+// Material components
+import { Grid } from '@material-ui/core';
+
+// Shared layouts
+import { Dashboard as DashboardLayout } from 'layouts';
+
+// Custom components
+import { Notifications, Password } from './components';
+
+// Component styles
+const styles = theme => ({
+  root: {
+    padding: theme.spacing.unit * 4
+  }
+});
+
+class Youtube extends Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <DashboardLayout title="Youtube">
+        <div className={classes.root}>
+         <Iframe
 				url="http://www.youtube.com/embed/xDMP3i36naA"
 				width="450px"
 				height="450px"
@@ -15,8 +42,18 @@ class Youtube extends PureComponent {
 				position="relative"
 				allowFullScreen
 			/>
-		)
-	}
+        </div>
+      </DashboardLayout>
+    );
+  }
 }
 
-ReactDOM.render(<Youtube />, document.body)
+Youtube.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Youtube);
+
+
+
+
