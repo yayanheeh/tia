597b2844c327713e7f705431dacfea981218ef66
yayanheeh <tokoxp58@gmail.com>
2020-02-26 11:27:07 +0700

    Update index.jsx

--- src/views/Youtube/components/index.jsx
+++ src/views/Youtube/components/index.jsx
@@ -1,38 +1,11 @@
-import React from 'react';
-
+import React, { PureComponent } from "react"
 import ReactDOM from "react-dom"
-import Iframe from "./iframe"
+import Iframe from "./iframe.js"
 
-// Externals
-import PropTypes from 'prop-types';
-
-// Material helpers
-import { withStyles } from '@material-ui/core';
-
-// Material components
-import { Grid } from '@material-ui/core';
-
-// Shared layouts
-import { Dashboard as DashboardLayout } from 'layouts';
-
-// Custom components
-import { Notifications, Password } from './components';
-
-// Component styles
-const styles = theme => ({
-  root: {
-    padding: theme.spacing.unit * 4
-  }
-});
-
-class Youtube extends Component {
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <DashboardLayout title="Youtube">
-        <div className={classes.root}>
-         <Iframe
+class Youtube extends PureComponent {
+	render() {
+		return (
+			<Iframe
 				url="http://www.youtube.com/embed/xDMP3i36naA"
 				width="450px"
 				height="450px"
@@ -42,18 +15,8 @@ class Youtube extends Component {
 				position="relative"
 				allowFullScreen
 			/>
-        </div>
-      </DashboardLayout>
-    );
-  }
+		)
+	}
 }
 
-Youtube.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(Youtube);
-
-
-
-
+ReactDOM.render(<Youtube />, document.body)
