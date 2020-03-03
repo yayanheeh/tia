74a477ccdfffdd2873f4fcfc8916d270c569788a
yayanheeh <tokoxp58@gmail.com>
2020-03-03 19:15:39 +0700

    Update index.jsx

--- src/views/Icons/index.jsx
+++ src/views/Icons/index.jsx
@@ -15,11 +15,8 @@ const styles = theme => ({
     padding: theme.spacing.unit * 4
   },
   iframe: {
-    position: 'absolute',
-    top: '0',
-    left: '0',
     width: '100%',
-    height: '100%,
+    minHeight: '640px',
     border: 0
   }
 });
