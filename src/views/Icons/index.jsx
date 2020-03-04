864f3a3577b043d8f4e2e2fd063e61c48d886ad2
yayanheeh <tokoxp58@gmail.com>
2020-03-04 13:19:24 +0700

    Update index.jsx

--- src/views/Icons/index.jsx
+++ src/views/Icons/index.jsx
@@ -6,7 +6,7 @@ import GridListTileBar from '@material-ui/core/GridListTileBar';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
-import tileData from './tileData';
+import tileData from 'tileData';
 
 const useStyles = makeStyles(theme => ({
   root: {
