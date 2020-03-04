d63b1f04f75ef74f39fcd0af40b01730d8c5c999
yayanheeh <tokoxp58@gmail.com>
2020-03-04 09:52:08 +0700

    Update index.jsx

--- src/views/Icons/index.jsx
+++ src/views/Icons/index.jsx
@@ -1,48 +1,62 @@
-import React, { Component } from 'react';
 
-// Externals
+import React from 'react';
 import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Slide from '@material-ui/core/Slide';
 
-// Material helpers
-import { withStyles } from '@material-ui/core';
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
 
-// Shared layouts
-import { Dashboard as DashboardLayout } from 'layouts';
-
-// Component styles
-const styles = theme => ({
-  root: {
-    padding: theme.spacing.unit * 4
-  },
-  iframe: {
-    width: '100%',
-    minHeight: '640px',
-    border: 0
-  }
-});
-
-class Icons extends Component {
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <DashboardLayout title="Icons">
-        <div className={classes.root}>
-          <iframe
-            className={classes.iframe}
-            src="https://material.io/tools/icons/?icon=accessibility&style=outline"
-            title="Material Design icons"
-          />
-        </div>
-
-
-      </DashboardLayout>
-    );
-  }
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
 }
 
-Icons.propTypes = {
-  classes: PropTypes.object.isRequired
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
 };
 
-export default withStyles(styles)(Icons);
+export default function HideAppBar(props) {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <HideOnScroll {...props}>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6">Scroll to Hide App Bar</Typography>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Toolbar />
+      <Container>
+        <Box my={2}>
+          {[...new Array(12)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+            )
+            .join('\n')}
+        </Box>
+      </Container>
+    </React.Fragment>
+  );
+}
