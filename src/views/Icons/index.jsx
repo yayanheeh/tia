2b89d455af3a4792bb0953a6c2253110961833d4
yayanheeh <tokoxp58@gmail.com>
2020-03-04 13:10:54 +0700

    Update index.jsx

--- src/views/Icons/index.jsx
+++ src/views/Icons/index.jsx
@@ -6,8 +6,7 @@ import GridListTileBar from '@material-ui/core/GridListTileBar';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
-import tileData from './tileData';
-
+import './titleData.js';
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
